55cc641c7b73f56c74556f0874d439d203a55772
ecizen <hardiektatendta@gmail.com>
2024-08-24 18:49:05 +0700

    deploy

--- src/constant/data-image.js
+++ src/constant/data-image.js
@@ -1,4 +1,4 @@
-import star from '../assets/Image/star.svg'
+import star from '../assets/Image/Star.svg'
 import ShoppingCart from '../assets/Image/ShoppingCart.svg'
 import CoupunApply from '../assets/Image/applycouponcode.svg'
 const images = {
